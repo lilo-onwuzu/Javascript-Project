31f73a2652199379c64fd78d65a9e95afcff6a96
Lilo Onwuzu <lpr422@gmail.com>
2016-03-25 14:23:43 -0700

    It will allow you to submit the form

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function(){
+  $(".showQuiz").click(function(){
+    $(".marketing").show(); //shows the row section when you click.Toggle not needed in this case because the hide term button takes care of the next click
+    $(".hideQuiz").show(); //shows the hidden "Click to hide" button
+    $(".showQuiz").hide(); //hides the "Click to learn" after you click on it
+  });
+  $("p.hideQuiz").click(function(){
+    $(".marketing").hide(); //shows & hides the row section when you click
+    $(".hideQuiz").hide(); //hides the "Click to Hide" after you click on it
+    $(".showQuiz").show(); //displays the "Click to Show" again
+  });
+  $(".lightBackground").click(function(){
+    $("body").removeClass();
+    $("body").addClass("lightPage");
+  });
+  $(".darkBackground").click(function(){
+    $("body").removeClass();
+    $("body").addClass("darkPage");
+  });
+  $(".active").click(function(){
+    $("body").removeClass();
+    $("body").addClass("defaultPage");
+  });
+
+  $("form#quiz").submit(function(event){
+    event.preventDefault();
+    var yourName = $("input#name").val();
+    var yourDrink = $("input:radio[name=drink]:checked").val();
+    var yourHobby = $("#hobby").val();
+    var yourKids = $("input:radio[name=kids]:checked").val();
+
+    if (yourDrink = 0){
+      var drinkName = "Coffee";
+      var result = "Italy";
+    };
+    if (yourDrink = 1){
+      var drinkName = "Whiskey";
+      var result = "Scotland";
+    };
+    if (yourDrink = 2){
+      var drinkName = "Wine";
+      var result = "Bordeaux";
+    };
+
+    if (yourName){
+        $(".out").show();
+
+        $(".addResult").text(result).val();
+        $(".addName").text(yourName).val();
+        $(".addHobby").text(yourHobby).val();
+        $(".addDrink").text(drinkName).val();
+        }
+    else {
+      alert("Please enter your name.");
+    };
+
+});
+});
